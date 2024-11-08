24164333add0b46908eedee8ffe8b1394d76d059
Jaya <akulajayateja4444@gmail.com>
2024-11-08 20:24:59 +0530

    Created new screen for quiz in react native fundamentals section. Instead of creating new screens for each question now one screen is used and the question and its content is rendered on it.

--- src/Screens/RNScreens/RN_Screen_Fluid.tsx
+++ src/Screens/RNScreens/RN_Screen_Fluid.tsx
@@ -1,5 +1,4 @@
-/* eslint-disable react-native/no-inline-styles */
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   StyleSheet,
@@ -7,21 +6,66 @@ import {
   StatusBar,
   Dimensions,
   Alert,
+  TouchableOpacity,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 import ProgressBar from '../../Components/ProgressBar';
+import {ReactNative_Fundamentals_Quiz_Questions} from '../../Components/ReactNative_Components/ReactNative_Quiz_Questions';
+import {useDispatch} from 'react-redux';
+import {
+  decrement,
+  increment,
+} from '../../../redux store/features/progressBarSlice';
 
-const {height, width} = Dimensions.get('window');
+const {height} = Dimensions.get('window');
 
 const RN_Fluid_Screen = () => {
+  const [questionCount, setQuestionCount] = useState(0);
+  const [result, setResult] = useState(false);
+  const [optionSelected, setOptionSelected] = useState(null);
+  const [disableOption, setDisableOption] = useState(false);
+
+  const dispatch = useDispatch();
+
+  const handleOptionSelected = (option: any) => {
+    setOptionSelected(option);
+    setResult(!result);
+    setDisableOption(!disableOption);
+  };
+
+  const handleContinueButton = () => {
+    if (questionCount + 1 < ReactNative_Fundamentals_Quiz_Questions.length) {
+      setQuestionCount(questionCount + 1);
+      dispatch(increment());
+      setOptionSelected(null);
+      setResult(false);
+      setDisableOption(false);
+    } else {
+      Alert.alert('The end bro!');
+      setQuestionCount(0);
+      dispatch(decrement());
+      setOptionSelected(null);
+      setResult(false);
+      setDisableOption(false);
+    }
+  };
+
+  const correctAnswer =
+    ReactNative_Fundamentals_Quiz_Questions[questionCount].correctAnswer;
+  const explanation =
+    ReactNative_Fundamentals_Quiz_Questions[questionCount].explanation;
+  const referenceLink =
+    ReactNative_Fundamentals_Quiz_Questions[questionCount].referenceLink;
+
   return (
-    <View style={{flex: 1, padding: 10, backgroundColor: ''}}>
+    <View style={RN_Fluid_Screen_Styles.Container}>
       <StatusBar backgroundColor={'black'} />
       <View style={RN_Fluid_Screen_Styles.TitleViewContainer}>
         <Text style={RN_Fluid_Screen_Styles.screenTitleText}>React Native</Text>
       </View>
+      {/* X icon, Progress bar and heart icon */}
       <View style={RN_Fluid_Screen_Styles.X_ProgressBar_Aura_container}>
-        <View style={{flex: 1}}>
+        <View style={RN_Fluid_Screen_Styles.X_Icon_View}>
           <Icon
             name="x"
             color={'black'}
@@ -29,41 +73,88 @@ const RN_Fluid_Screen = () => {
             onPress={() => Alert.alert('hey!')}
           />
         </View>
-        <View
-          style={{
-            flex: 10,
-            width: 500,
-            marginLeft: 10,
-            // justifyContent: 'center',
-            // alignItems: 'center',
-            // borderWidth: 1,
-          }}>
+        <View style={RN_Fluid_Screen_Styles.ProgressBar_View}>
           <ProgressBar />
         </View>
-        <View
-          style={{
-            flex: 1,
-            width: 50,
-            // borderWidth: 1,
-            marginLeft: 10,
-            justifyContent: 'center',
-            alignItems: 'flex-end',
-          }}>
+        <View style={RN_Fluid_Screen_Styles.HeartIcon_View}>
           <Icon name="heart" size={30} color={'red'} solid />
         </View>
       </View>
-      <Text style={{fontSize: 16, color: 'black'}}>Hi!</Text>
-      <View>
-        <Text>
-          The screen height and width is :{' '}
-          {`${height.toFixed(0)} & ${width.toFixed(0)}`}
+      {/* The question view */}
+      <View style={RN_Fluid_Screen_Styles.QuestionContainer}>
+        <Text style={RN_Fluid_Screen_Styles.QuestionText}>
+          {ReactNative_Fundamentals_Quiz_Questions[questionCount].question}
         </Text>
+        <View style={RN_Fluid_Screen_Styles.OptionsContainer}>
+          {ReactNative_Fundamentals_Quiz_Questions[questionCount].options.map(
+            (option, index) => (
+              <View key={index} style={RN_Fluid_Screen_Styles.OptionView}>
+                <View style={RN_Fluid_Screen_Styles.OptionNumberView}>
+                  <Text style={RN_Fluid_Screen_Styles.OptionNumberText}>
+                    {index + 1}
+                    {'.)'}
+                  </Text>
+                </View>
+                <View style={RN_Fluid_Screen_Styles.OptionButtonView}>
+                  <TouchableOpacity
+                    style={[
+                      RN_Fluid_Screen_Styles.DefaultOptionButtonStyle,
+                      optionSelected === option
+                        ? option === correctAnswer
+                          ? RN_Fluid_Screen_Styles.CorrectOptionButtonStyle
+                          : RN_Fluid_Screen_Styles.WrongOptionButtonStyle
+                        : null,
+                    ]}
+                    onPress={() => handleOptionSelected(option)}
+                    disabled={disableOption}>
+                    <Text style={RN_Fluid_Screen_Styles.OptionText}>
+                      {option}
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ),
+          )}
+          {result && (
+            <View style={RN_Fluid_Screen_Styles.ResultExplanationContainer}>
+              <View
+                style={
+                  optionSelected === correctAnswer
+                    ? RN_Fluid_Screen_Styles.CorrectAnswerExplanation
+                    : RN_Fluid_Screen_Styles.WrongAnserExplanation
+                }>
+                <Text style={RN_Fluid_Screen_Styles.ExplanationText}>
+                  • {explanation}
+                </Text>
+                <Text style={RN_Fluid_Screen_Styles.ExplanationText}>
+                  • For further reference: {referenceLink}
+                </Text>
+              </View>
+              <TouchableOpacity
+                style={
+                  optionSelected === correctAnswer
+                    ? RN_Fluid_Screen_Styles.ContinueButtonIfCorrect
+                    : RN_Fluid_Screen_Styles.ContinueButtonIfWrong
+                }
+                onPress={() => handleContinueButton()}>
+                <Text style={RN_Fluid_Screen_Styles.ContinueButtonText}>
+                  Continue
+                </Text>
+              </TouchableOpacity>
+            </View>
+          )}
+        </View>
       </View>
     </View>
   );
 };
 
 const RN_Fluid_Screen_Styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: 'white',
+  },
   TitleViewContainer: {
     justifyContent: 'center',
     alignItems: 'center',
@@ -77,10 +168,127 @@ const RN_Fluid_Screen_Styles = StyleSheet.create({
   },
   X_ProgressBar_Aura_container: {
     height: height / 16,
-    // borderWidth: 1,
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'row',
+  },
+  X_Icon_View: {
+    flex: 1,
+  },
+  ProgressBar_View: {
+    flex: 10,
+    width: 500,
+    marginLeft: 10,
+  },
+  HeartIcon_View: {
+    flex: 1,
+    width: 50,
+    marginLeft: 10,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+  },
+  QuestionContainer: {
+    flex: 1,
+  },
+  QuestionText: {
+    fontSize: 16,
+    color: 'black',
+    fontFamily: 'Montserrat-SemiBold',
+  },
+  OptionsContainer: {
+    flex: 1,
+  },
+  OptionView: {
+    height: height / 14,
+    marginTop: 10,
+    flexDirection: 'row',
+  },
+  OptionNumberView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  OptionNumberText: {
+    fontSize: 16,
+    color: 'black',
+    fontFamily: 'Montserrat-SemiBold',
+  },
+  OptionButtonView: {
+    flex: 7,
+  },
+  DefaultOptionButtonStyle: {
+    height: height / 14,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    borderRadius: 8,
+    backgroundColor: 'white',
+    elevation: 3,
+    borderWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.2)',
+  },
+  CorrectOptionButtonStyle: {
+    backgroundColor: '#C7FFB7',
+  },
+  WrongOptionButtonStyle: {
+    backgroundColor: '#FFC5B2',
+  },
+  OptionText: {
+    fontSize: 16,
+    color: 'black',
+    fontFamily: 'Montserrat-SemiBold',
+  },
+  ResultExplanationContainer: {
+    flex: 1,
+    height: height / 2,
+    borderRadius: 8,
+    marginTop: 10,
+  },
+  CorrectAnswerExplanation: {
+    flex: 1,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 8,
+    backgroundColor: '#C7FFB7',
+    borderColor: 'rgba(147, 250, 165, 0.8)',
+  },
+  WrongAnserExplanation: {
+    flex: 1,
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 8,
+    backgroundColor: '#FFC5B2',
+    borderColor: 'rgba(250, 0, 0, 0.2)',
+  },
+  ExplanationText: {
+    fontSize: 14,
+    color: 'rgba(0, 0, 0, 0.6)',
+    fontFamily: 'Montserrat-SemiBold',
+  },
+  ContinueButtonIfCorrect: {
+    height: height / 14,
+    borderRadius: 8,
+    marginTop: 10,
+    borderWidth: 1,
+    elevation: 3,
+    backgroundColor: '#6FE427',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  ContinueButtonIfWrong: {
+    height: height / 14,
+    borderRadius: 8,
+    marginTop: 10,
+    borderWidth: 1,
+    backgroundColor: '#FF2A12',
+    elevation: 3,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  ContinueButtonText: {
+    fontSize: 16,
+    color: 'white',
+    fontFamily: 'Montserrat-SemiBold',
   },
 });
 
