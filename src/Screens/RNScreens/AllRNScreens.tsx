d15ec3dc195954977b08620d86f12828f4fd4120
Jaya <akulajayateja4444@gmail.com>
2024-12-02 17:00:05 +0530

    fetching the fundamentals quiz part from the backend worked!

--- src/Screens/RNScreens/AllRNScreens.tsx
+++ src/Screens/RNScreens/AllRNScreens.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import ReactNative_Main_Screen from './RN_Main_Screen';
 import {
@@ -11,6 +11,28 @@ import RN_Fluid_Screen from '../Fluid_Screen';
 const Stack = createNativeStackNavigator();
 
 const AllRNScreens = () => {
+  const [questions, setQuestions] = useState([]);
+
+  const getReactNativeQuizData = async () => {
+    try {
+      const response = await fetch(
+        'http://192.168.0.5:3000/quiz/ReactNative/Fundamentals',
+      );
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      const data = await response.json();
+      console.log(data);
+      setQuestions(data);
+    } catch (error) {
+      console.error(`Error fetching quiz data: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    getReactNativeQuizData();
+  }, []);
+
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen
@@ -21,7 +43,8 @@ const AllRNScreens = () => {
         name="RN_Fundamentals_quiz_question"
         component={RN_Fluid_Screen}
         initialParams={{
-          quizQuestions: ReactNative_Fundamentals_Quiz_Questions,
+          // quizQuestions: ReactNative_Fundamentals_Quiz_Questions,
+          quizQuestions: questions,
           QuizTitle: 'React Native',
         }}
       />
