3e4d11fc34ad85c76f7841869abc9e7b108a6702
Jaya <akulajayateja4444@gmail.com>
2025-09-10 23:15:44 +0530

    added a loading indicator after signing in

--- src/Screens/SignInScreen.tsx
+++ src/Screens/SignInScreen.tsx
@@ -8,8 +8,13 @@ import {
   TouchableOpacity,
   TextInput,
   Alert,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
 } from 'react-native';
 import Auth from './Auth';
+
+const screenHeight = Dimensions.get('window').height;
 
 const LoginScreen = () => {
   const {email, setEmail, password, setPassword, loading, signInWithEmail} =
@@ -17,13 +22,13 @@ const LoginScreen = () => {
 
   const Navigation = useNavigation();
 
-  const handleLoginButton = () => {
+  const handleLoginButton = async () => {
     // if (email.length < 10) {
     //   Alert.alert('Come on! type the correct email address!');
     // }
+    await signInWithEmail();
     setEmail('');
     setPassword('');
-    signInWithEmail();
     // Alert.alert('You are signed in!');
   };
 
@@ -32,105 +37,113 @@ const LoginScreen = () => {
   };
 
   return (
-    <View style={{flex: 1, padding: 10, backgroundColor: 'white'}}>
-      <View
-        style={{
-          flex: 1,
-          // borderWidth: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-        <Text
+    <ScrollView contentContainerStyle={{flexGrow: 1}}>
+      <View style={{flex: 1, padding: 10, backgroundColor: 'white'}}>
+        <View
           style={{
-            color: 'orange',
-            fontStyle: 'italic',
-            fontSize: 28,
-            fontWeight: 'bold',
+            flex: 1,
+            // borderWidth: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
           }}>
-          Quizzy
-        </Text>
-      </View>
-      <View style={{flex: 2, justifyContent: 'center'}}>
-        <View style={{}}>
-          <Text style={{color: 'black', marginLeft: 10}}>Name</Text>
-          <TextInput
-            style={styles.InputStyle}
-            placeholder="Enter your username/email"
-            onChangeText={v => setEmail(v)}
-            value={email}
-            placeholderTextColor={'grey'}
-            autoCapitalize="none"
-          />
-        </View>
-        <View>
-          <Text style={{color: 'black', marginLeft: 10}}>Password</Text>
-          <TextInput
-            style={styles.InputStyle}
-            placeholder="Enter your password"
-            placeholderTextColor={'grey'}
-            onChangeText={p => setPassword(p)}
-            value={password}
-            autoCapitalize="none"
-          />
-        </View>
-        <View style={{alignItems: 'flex-end', marginBottom: 10}}>
           <Text
-            onPress={() => Alert.alert('You clicked forget password')}
-            style={{color: 'black'}}>
-            Forget password?
+            style={{
+              color: 'orange',
+              fontStyle: 'italic',
+              fontSize: 28,
+              fontWeight: 'bold',
+            }}>
+            Quizzy
           </Text>
         </View>
-        <View style={{}}>
-          <TouchableOpacity
-            style={{
-              // borderWidth: 1,
-              height: 50,
-              // width: 100,
-              elevation: 3,
-              borderRadius: 8,
-              marginLeft: 10,
-              marginRight: 10,
-              marginBottom: 10,
-              backgroundColor: 'skyblue',
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}
-            onPress={() => handleLoginButton()}
-            // onPress={() => Alert.alert('You pressed sign-in!')}
-            testID="Sign-in button">
-            <Text style={{color: 'black', fontSize: 24, fontWeight: 'bold'}}>
-              Sign in
+        <View style={{flex: 2, justifyContent: 'center'}}>
+          <View style={{}}>
+            <Text style={{color: 'black', marginLeft: 10}}>Name</Text>
+            <TextInput
+              style={styles.InputStyle}
+              placeholder="Enter your username/email"
+              onChangeText={v => setEmail(v)}
+              value={email}
+              placeholderTextColor={'grey'}
+              autoCapitalize="none"
+            />
+          </View>
+          <View>
+            <Text style={{color: 'black', marginLeft: 10}}>Password</Text>
+            <TextInput
+              style={styles.InputStyle}
+              placeholder="Enter your password"
+              placeholderTextColor={'grey'}
+              onChangeText={p => setPassword(p)}
+              value={password}
+              autoCapitalize="none"
+              secureTextEntry={true}
+            />
+          </View>
+          <View style={{alignItems: 'flex-end', marginBottom: 10}}>
+            <Text
+              onPress={() => Alert.alert('You clicked forget password')}
+              style={{color: 'black'}}>
+              Forget password?
             </Text>
-          </TouchableOpacity>
+          </View>
+          <View style={{}}>
+            {loading ? (
+              <ActivityIndicator size="large" color="orange" />
+            ) : (
+              <TouchableOpacity
+                style={{
+                  // borderWidth: 1,
+                  // height: 50,
+                  // width: 100,
+                  elevation: 3,
+                  borderRadius: 8,
+                  padding: 10,
+                  marginLeft: 10,
+                  marginRight: 10,
+                  marginBottom: 10,
+                  backgroundColor: 'black',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
+                onPress={() => handleLoginButton()}
+                testID="Sign-in button">
+                <Text
+                  style={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>
+                  Sign in
+                </Text>
+              </TouchableOpacity>
+            )}
+          </View>
         </View>
-      </View>
-      {/* <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        {/* <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Text style={{color: 'black'}}>or sign-in with</Text>
       </View> */}
-      <View style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}>
-        <Text style={{color: 'black'}}>
-          Don't have an account?
-          <Text onPress={() => handleSignUpScreen()} style={{color: 'blue'}}>
-            {' '}
-            Sign up
-          </Text>{' '}
-        </Text>
+        <View
+          style={{flex: 1, justifyContent: 'flex-end', alignItems: 'center'}}>
+          <Text style={{color: 'black'}}>
+            Don't have an account?
+            <Text onPress={() => handleSignUpScreen()} style={{color: 'blue'}}>
+              {' '}
+              Sign up
+            </Text>{' '}
+          </Text>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   InputStyle: {
     borderWidth: 1,
-    opacity: 0.2,
+    // opacity: 0.2,
+    borderColor: 'lightgrey',
+    height: screenHeight / 14,
     color: 'black',
     borderRadius: 8,
-    marginLeft: 10,
-    marginRight: 10,
-    marginBottom: 20,
+    margin: 10,
     padding: 10,
-    // backgroundColor: 'lightgrey',
   },
 });
 
--- src/Screens/SignUpScreen.tsx
+++ src/Screens/SignUpScreen.tsx
@@ -1,9 +1,19 @@
 /* eslint-disable react-native/no-inline-styles */
 import {useNavigation} from '@react-navigation/native';
 import React, {useState} from 'react';
-import {Text, View, Alert, TextInput, StyleSheet} from 'react-native';
+import {
+  Text,
+  View,
+  Alert,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+  ScrollView,
+} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import Auth from './Auth';
+
+const screenHeight = Dimensions.get('window').height;
 
 const SignUpScreen = () => {
   const [userName, setUsername] = useState('');
@@ -28,95 +38,120 @@ const SignUpScreen = () => {
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        padding: 10,
-        backgroundColor: 'white',
-      }}>
-      <View style={{flex: 1, justifyContent: 'flex-end'}}>
-        <Text style={{color: 'black', fontSize: 18}}>
-          Hello! Please register yourself to get started.
-        </Text>
-      </View>
-      <View style={{flex: 3, justifyContent: 'center'}}>
-        <View>
-          <TextInput
-            style={SignUpScreenStyles.inputStyle}
-            value={userName}
-            placeholder="Enter your username"
-            onChangeText={u => setUsername(u)}
-            placeholderTextColor={'black'}
-          />
-        </View>
-        <View>
-          <TextInput
-            style={SignUpScreenStyles.inputStyle}
-            value={email}
-            placeholder="Enter your email"
-            onChangeText={e => setEmail(e)}
-            placeholderTextColor={'black'}
-          />
-        </View>
-        <View>
-          <TextInput
-            style={SignUpScreenStyles.inputStyle}
-            value={password}
-            placeholder="Enter your password"
-            onChangeText={p => setPassword(p)}
-            placeholderTextColor={'black'}
-          />
-        </View>
-        <View>
-          <TextInput
-            style={SignUpScreenStyles.inputStyle}
-            placeholder="Confirm password"
-            placeholderTextColor={'black'}
-          />
-        </View>
-        <View>
-          <TouchableOpacity
-            style={{
-              // borderWidth: 1,
-              elevation: 5,
-              borderRadius: 8,
-              padding: 10,
-              alignItems: 'center',
-              marginTop: 20,
-              marginLeft: 10,
-              marginRight: 10,
-              backgroundColor: 'lightblue',
-            }}
-            onPress={() => handleSignUpScreen()}>
-            <Text style={{color: 'black', fontSize: 20, fontWeight: 'bold'}}>
-              Sign up!
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
+    <ScrollView
+      keyboardShouldPersistTaps="handled"
+      contentContainerStyle={{flexGrow: 1}}>
+      {/* <TouchableWithoutFeedback onPress={Keyboard.dismiss}> */}
       <View
         style={{
-          flex: 2,
-          // borderWidth: 1,
-          justifyContent: 'flex-end',
-          alignItems: 'center',
+          flex: 1,
+          padding: 10,
+          backgroundColor: 'white',
         }}>
-        <Text style={{color: 'black'}}>
-          Already have an account?
-          <Text onPress={() => handleSignIn()} style={{color: 'blue'}}>
-            {' '}
-            Sign in
+        <View
+          style={{
+            flex: 1,
+            // borderWidth: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 28,
+              color: 'orange',
+              fontStyle: 'italic',
+              fontWeight: 'bold',
+            }}>
+            Quizzy
           </Text>
-        </Text>
+        </View>
+        <View style={{flex: 1, justifyContent: 'flex-end'}}>
+          <Text style={{color: 'black', fontSize: 18}}>
+            Hello! Please register yourself to get started.
+          </Text>
+        </View>
+        <View style={{flex: 4, justifyContent: 'center'}}>
+          <View>
+            <TextInput
+              style={SignUpScreenStyles.inputStyle}
+              value={userName}
+              placeholder="Enter your username"
+              onChangeText={u => setUsername(u)}
+              placeholderTextColor={'black'}
+            />
+          </View>
+          <View>
+            <TextInput
+              style={SignUpScreenStyles.inputStyle}
+              value={email}
+              placeholder="Enter your email"
+              onChangeText={e => setEmail(e)}
+              placeholderTextColor={'black'}
+            />
+          </View>
+          <View>
+            <TextInput
+              style={SignUpScreenStyles.inputStyle}
+              value={password}
+              placeholder="Enter your password"
+              onChangeText={p => setPassword(p)}
+              placeholderTextColor={'black'}
+            />
+          </View>
+          <View>
+            <TextInput
+              style={SignUpScreenStyles.inputStyle}
+              placeholder="Confirm password"
+              placeholderTextColor={'black'}
+            />
+          </View>
+          <View>
+            <TouchableOpacity
+              style={{
+                // borderWidth: 1,
+                elevation: 5,
+                borderRadius: 8,
+                padding: 10,
+                alignItems: 'center',
+                marginTop: 20,
+                marginLeft: 10,
+                marginRight: 10,
+                backgroundColor: 'black',
+              }}
+              onPress={() => handleSignUpScreen()}>
+              <Text style={{color: 'white', fontSize: 20, fontWeight: 'bold'}}>
+                Sign up
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View
+          style={{
+            flex: 2,
+            // borderWidth: 1,
+            justifyContent: 'flex-end',
+            alignItems: 'center',
+          }}>
+          <Text style={{color: 'black'}}>
+            Already have an account?
+            <Text onPress={() => handleSignIn()} style={{color: 'blue'}}>
+              {' '}
+              Sign in
+            </Text>
+          </Text>
+        </View>
       </View>
-    </View>
+      {/* </TouchableWithoutFeedback> */}
+    </ScrollView>
   );
 };
 
 const SignUpScreenStyles = StyleSheet.create({
   inputStyle: {
     borderWidth: 1,
-    opacity: 0.2,
+    // opacity: 0.2,
+    borderColor: 'lightgrey',
+    height: screenHeight / 14,
     color: 'black',
     borderRadius: 8,
     margin: 10,
